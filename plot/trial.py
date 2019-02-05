52dcf0d1039de34812c1c91369db6b7d57594c87
=Isaac <=trevex2000@live.com>
2019-02-05 16:21:02 +0300

    ignore test files

--- plot/trial.py
+++ plot/trial.py
@@ -1,13 +1,49 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 
-fig = plt.figure()  # an empty figure with no axes
-fig.suptitle('No axes on this figure')  # Add a title so we know which it is
+#created classes
+from paper_space import PaperSpace
 
-fig, ax_lst = plt.subplots(2, 2)  # a figure with a 2x2 grid of Axes
+class PlotWindMap:
+    def __init__(self):
+        plt.axis('off')
+        self.paper_space = PaperSpace()
+        pass
+    
+    def plotMap(self):
+        axes = plt.gca()
+        axes.set_xlim(self.paper_space.x_limit)
+        axes.set_ylim(self.paper_space.x_limit)
+        rect = self.getRect((10,30), 300, 150, 0.5)
+        self.addText("Fig. 27.4-4, ≤ 0.5L", 150)
+        self.addText("θ", 100)
+        self.addText("0˚", 50)
+        plt.gca().add_patch(rect)
+        plt.show()
+        pass
 
-a = pandas.DataFrame(np.random.rand(4,5), columns = list('abcde'))
-a_asndarray = a.values
+    def getRect(self, start_point, length, width, 
+        linewidth=1, edgecolor='black', facecolor='none'):
 
-b = np.matrix([[1,2],[3,4]])
-b_asarray = np.asarray(b)
+        return plt.Rectangle(start_point,length,width,
+            linewidth=linewidth, edgecolor=edgecolor, facecolor=facecolor)
+
+    def addText(self, text, y):
+        return plt.text(20, y, text)
+
+plot_map = PlotWindMap()
+plot_map.plotMap()
+
+
+
+
+
+# plt.plot((1,2,3), (5,7,4))
+
+# fig, ax = plt.subplots(1)
+# plt.savefig("test.png", bbox_inches='tight')
+
+# ax.add_patch(rect)
+# ax.axis('off')
+
