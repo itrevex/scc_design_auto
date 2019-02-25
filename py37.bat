b8f27e0883708c938b62c7f649e1571a0523d30d
=Isaac <=trevex2000@live.com>
2019-02-25 17:35:50 +0300

    ignore exe files

--- py37.bat
+++ py37.bat
@@ -8,6 +8,7 @@ REM git config --local --bool core.bare false
 REM py -3.7-64 plot/trial.py 
 REM py -3.7-64 main.py "project/project.json"
 REM iscc "setup/design_auto.iss"
-"setup/setups/ssc_design_setup-1.0.2"
+REM "setup/setups/ssc_design_setup-1.0.2"
 REM "dist/main/trsc"
-REM py -3.7-64 -m pip freeze rem show all packages installed 
+REM py -3.7-64 -m pip freeze rem show all packages installed
+git rm --cached *.exe 
