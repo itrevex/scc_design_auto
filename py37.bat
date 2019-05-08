d9359c673338286f3011a5a4ffee04a7259fdd89
=Isaac <=trevex2000@live.com>
2019-05-08 14:29:32 +0300

    update version

--- py37.bat
+++ py37.bat
@@ -9,7 +9,7 @@ REM py -3.7-64 plot/trial.py
 REM compile code
 REM py -3.7-64 -m PyInstaller main.spec
 REM iscc "setup/design_auto.iss"
-REM "setup/setups/ssc_design_setup-1.0.6"
+REM "setup/setups/ssc_design_setup-1.0.7"
 
 REM "dist/main/trsc"
 REM py -3.7-64 -m pip freeze rem show all packages installed
