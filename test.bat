895d9353dffa27afce01c1fb67ddd78971b2d925
=Isaac <=trevex2000@live.com>
2019-06-04 07:11:30 +0300

    test dxf input

--- test.bat
+++ test.bat
@@ -4,7 +4,7 @@ REM py -3.7-64 -m coverage run -m main.py "input/philip.trad"
 REM py -3.7-64 -m pytest --help
 REM py -3.7-64 -m pytest --cov-report html  
 REM py -3.7-64 -m coverage run --source=/src/common/*.py
-py -3.7-64 -m pytest  -s
+py -3.7-64 -m pytest  src/tests/test_dxf -v
 REM py -3.7-64 -m coverage html 
 
 
